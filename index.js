8c07550e89f76801c34ac2d6f33ea1ae615bba1d
Phuoc <khuathoduyphuoc2001@gmail.com>
2023-09-18 21:29:34 +0700

    Chỉnh sửa mô hình 3 khối

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 //Bài 1: Tính tiền lương nhân viên. Viết chương trình tính tiền lương nhân viên. Lương 1 ngày: 100.000 .Cho người dùng nhập vào số ngày làm.
 function tinhLuong() {
-    //INPUT: nhập vào số ngày làm và lấy qua id có giá trị là soNgayLam. 
+    //INPUT: nhập vào số ngày làm.
     var soNgayLam = document.getElementById('soNgayLam').value;
     //Khai báo biến luong1Ngay và lấy qua id có giá trị là soNgayLam.
     var luong1Ngay = document.getElementById('luong1Ngay').value;
-    //Khai báo biến output1 có giá trị bằng 0.
-    var ouput1 = 0;
     //PROGRESS:
-    //B1: Khai báo biến ketQuaTinhLuong và lấy qua id có giá trị là ketQuaTinhLuong
+    //B1: Khai báo biến output1 có giá trị bằng 0.
+    var ouput1 = 0;
+    //B2: Khai báo biến ketQuaTinhLuong và lấy qua id có giá trị là ketQuaTinhLuong
     var ketQuaTinhLuong = document.getElementById('ketQuaTinhLuong');
-    //B2: sử dụng công thức ouput1 = soNgayLam * luong1Ngay.
+    //B3: sử dụng công thức ouput1 = soNgayLam * luong1Ngay.
     ouput1 = soNgayLam * luong1Ngay;
-    //B3: Xuất biến ketQuaTinhLuong ra màn hình có giá trị là output1.
+    //OUTPUT: Kết quả tiền lương nhân viên
     ketQuaTinhLuong.innerHTML = ouput1.toLocaleString() + "" + "VND";
 }
 //Bài 2: Tính giá trị trung bình. Viết chương trình nhập vào 5 số thực. Tính giá trị trung bình của 5 số này và xuất ra màn hình.
@@ -23,14 +23,14 @@ function clickSoThuc() {
     var nhapSoThuc3 = parseFloat(document.getElementById('nhapSoThuc3').value);
     var nhapSoThuc4 = parseFloat(document.getElementById('nhapSoThuc4').value);
     var nhapSoThuc5 = parseFloat(document.getElementById('nhapSoThuc5').value);
-    //OUTPUT: Khai báo biến output2 có giá trị bằng 0.
-    var output2 = 0;
     //PROGRESS:
-    //B1: khai báo biến ketQuaTrungBinh và lấy id có giá trị là ketQuaTrungBinh. 
+    //B1: Khai báo biến output2 có giá trị bằng 0.
+    var output2 = 0;
+    //B2: khai báo biến ketQuaTrungBinh và lấy id có giá trị là ketQuaTrungBinh. 
     var ketQuaTrungBinh = document.getElementById('ketQuaTrungBinh');
-    //B2: sử dụng công thức output2 = (nhapSoThuc1 + nhapSoThuc2 + nhapSoThuc3 + nhapSoThuc4 + nhapSoThuc5) / 5.
+    //B3: sử dụng công thức output2 = (nhapSoThuc1 + nhapSoThuc2 + nhapSoThuc3 + nhapSoThuc4 + nhapSoThuc5) / 5.
     output2 = (nhapSoThuc1 + nhapSoThuc2 + nhapSoThuc3 + nhapSoThuc4 + nhapSoThuc5) / 5;
-    //B3: Xuất ra ketQuaTrungBinh ra màn hình có giá trị là output2.
+    //OUTPUT: Kết quả trung bình của 5 số này và xuất ra màn hình.
     ketQuaTrungBinh.innerHTML = output2;
 }
 //Bài 3: Quy đổi tiền. Giá USD hiện nay là 23.500 VND. Viết chương trình quy đổi từ USD sang VND. Cho người dùng nhập vào số tiền USD. Tính và xuât ra số tiền sau quy đổi VND.
@@ -39,14 +39,14 @@ var nutQuyDoi = document.getElementById('nutQuyDoi');
 nutQuyDoi.onclick = function () {
     //INPUT: Nhập vào số tiền VND.
     var soTienUsd = document.getElementById('soTienUsd').value;
-    //OUTPUT: Khai báo biến output3 ban đầu có giá trị là 0.
-    var output3 = 0;
     //PROGRESS:
-    //B1: Khai báo biến tagspan3 và lấy qua id là soTienVnd.
+    //B1: Khai báo biến output3 ban đầu có giá trị là 0.
+    var output3 = 0;
+    //B2: Khai báo biến tagspan3 và lấy qua id là soTienVnd.
     var tagspan3 = document.getElementById('soTienVnd');
-    //B2: viết công thức output3 = sotienusd * 23500.
+    //B3: viết công thức output3 = sotienusd * 23500.
     output3 = soTienUsd * 23500;
-    //Xuất ra màn hình giá trị của biến tagspan3 bằng giá trị của biến output3.
+    //OUTPUT: Tính và xuât ra số tiền sau quy đổi VND
     tagspan3.value = output3.toLocaleString();
 }
 // Bài 4: Tính diện tích, chu vi hình chữ nhật. Viết chương trình nhập vào 2 chiều dài và rộng của HCN (hình chữ nhật).
@@ -55,14 +55,14 @@ function clickDienTich() {
     //INPUT: Nhập vào chiều dài và rộng.
     var chieuDai = document.getElementById('chieuDai');
     var chieuRong = document.getElementById('chieuRong');
-    //OUTPUT: Khai báo biến ban đầu output4a = 0;
-    var output4a = 0;
     // PROGRESS:
-    //B1: Khai báo biến tagspan4a và lấy qua id có giá trị là ketQuaDienTich. 
+    //B1: Khai báo biến ban đầu output4a = 0;
+    var output4a = 0;
+    //B2: Khai báo biến tagspan4a và lấy qua id có giá trị là ketQuaDienTich. 
     var tagspan4a = document.getElementById('ketQuaDienTich');
-    //B2: Sử dụng công thức tính diện tích là chieuDai * chieuRong;
+    //B3: Sử dụng công thức tính diện tích là chieuDai * chieuRong;
     output4a = chieuDai.value * chieuRong.value;
-    //B3: Xuất biến tagspan4a ra màn hình bằng biến output4a.
+    //OUTPUT: Kết quả diện tích hình chữ nhật.
     tagspan4a.innerHTML = output4a;
 }
 //Khai báo hàm sự kiện onclick có giá trị là clickchuvi()
@@ -70,14 +70,14 @@ function clickChuVi() {
     //INPUT: Nhập vào chiều dài và rộng.
     var chieuDai = document.getElementById('chieuDai');
     var chieuRong = document.getElementById('chieuRong');
-    //OUTPUT: Khai báo biến ban đầu output4b = 0;
+    // PROGRESS: 
+    //B1: Khai báo biến ban đầu output4b = 0;
     var output4b = 0;
-    // progress
-    //B1: Khai báo biến tagspan4b và lấy qua id có giá trị là ketQuaDienTich.
+    //B2: Khai báo biến tagspan4b và lấy qua id có giá trị là ketQuaDienTich.
     var tagspan4b = document.getElementById('ketQuaChuVi');
-    //B2: Sử dụng công thức tính chu vi là (chieuDai + chieuRong) *2;
+    //B3: Sử dụng công thức tính chu vi là (chieuDai + chieuRong) *2;
     output4b = (chieuDai.value * 1 + chieuRong.value * 1) * 2;
-    //B3: Xuất biến tagspan4b ra màn hình bằng biến output4b.
+    //OUTPUT Kết quả chu vi hình chữ nhật.
     tagspan4b.innerHTML = output4b;
 }
 //Bài 5: Tính tổng 2 ký số.Viết chương trình nhập vào 1 số có 2 chữ số (VD: 12,44,83). Tính tổng 2 ký số vừa nhập.
@@ -85,17 +85,17 @@ function clickChuVi() {
 var tong2KySo = document.getElementById('tong2KySo').onclick = function () {
     //INPUT: Nhập vào 1 số có hai chữ số.
     var nhap1So = Number(document.getElementById('nhap1So').value);
-    //OUTPUT: Khai báo biến output5 ban đầu = 0. 
-    var output5 = 0;
     //PROGRESS: 
-    //B1: Khai báo biến ketquaTong và lấy qua id là ketquaTong.
+    //B1: Khai báo biến output5 ban đầu = 0. 
+    var output5 = 0;
+    //B2: Khai báo biến ketquaTong và lấy qua id là ketquaTong.
     var ketQuaTong = document.getElementById('ketQuaTong');
-    //B2: Khai báo biến hangChuc và sử dụng hàm Math.floor(nhap1So / 10) lấy số nguyên.
+    //B3: Khai báo biến hangChuc và sử dụng hàm Math.floor(nhap1So / 10) lấy số nguyên.
     var hangChuc = Math.floor(nhap1So / 10);
-    //B3: Khai báo biến hangDonVi và sử dụng hàm Math.floor(nhap1So % 10) lấy số nguyên.
+    //B4: Khai báo biến hangDonVi và sử dụng hàm Math.floor(nhap1So % 10) lấy số nguyên.
     var hangDonVi = Math.floor(nhap1So % 10);
-    //B4: Tạo công thức cho output5 = hangChuc + hangDonVi.
+    //B5: Tạo công thức cho output5 = hangChuc + hangDonVi.
     output5 = hangChuc + hangDonVi;
-    //B5: Xuất biến ketQuaTong ra màn hình bằng biến output5.
+    //B5: Kết quả tổng 2 ký số.
     ketQuaTong.innerHTML = output5;
 }
